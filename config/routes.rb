166a1a4a7b2d1823f0ce5ebaaa35a6b9c7d7abe8
Dasha Murauyova <dmurauyova@dxagency.com>
2019-08-13 15:13:43 -0400

    changed the ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "home#index"
 
-	Rails.application.routes.draw do
-	  devise_for :users, controllers: {
-	    sessions: 'users/sessions'
-	  }
-	end
+	# Rails.application.routes.draw do
+	#   devise_for :users, controllers: {
+	#     sessions: 'users/sessions'
+	#   }
+	# end
 end
